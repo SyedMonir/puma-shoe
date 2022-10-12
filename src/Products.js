4f505b16eae05cbcdc24613f4fbe3aa3f1e71f07
Syed Monir <Rambling.ctg@gmail.com>
2022-10-13 00:14:47 +0600

    fixed mobile view

--- src/Products.js
+++ src/Products.js
@@ -16,31 +16,31 @@ const Products = () => {
 
         <div
           tabIndex={0}
-          className="collapse collapse-arrow border mt-5 rounded-box"
+          className="collapse collapse-arrow border mt-5 rounded-box shadow-md"
         >
-          <div className="collapse-title text-xl font-medium">
+          <div className="collapse-title text-xl font-medium p-3 shadow-2xl">
             <div className="card card-side">
               <figure className="sm:w-24 pr-2 sm:pr-0 ">
                 <img src="https://placeimg.com/200/280/arch" alt="Movie" />
               </figure>
-              <div className="card-body p-0 sm:p-[2rem]">
+              <div className="card-body p-0 sm:p-[2rem] gap-1">
                 <p className="font-medium text-base sm:text-lg">
                   Puma RS Fast Limiter Sneaker Mens Black Red
                 </p>
                 <p className="text-slate-600 font-light text-xs sm:text-base">
                   PUMA
                 </p>
-                <span className="text-sm w-fit p-1 rounded bg-green-200 text-green-700 font-bold">
+                <span className="text-sm w-fit p-1 rounded-md px-2 bg-green-200 text-green-700 font-bold">
                   Delivered
                 </span>
-              </div>
-            </div>
               <label
                 htmlFor="my-modal-6"
-                className="btn btn-sm my-auto mt-2 bg-primary text-white sm:hidden"
+                className="btn btn-sm my-auto mt-2 rounded py-2 bg-[#1750FF] border-0 text-white sm:hidden"
               >
-                Open Details
+                Order Details
               </label>
+              </div>
+            </div>
           </div>
           <div className="collapse-content">
             <p className="font-semibold">Order ID #123456</p>
@@ -52,19 +52,19 @@ const Products = () => {
         </div>
 
         {/* Mobile */}
-        {/* <!-- The button to open modal --> */}
-
-        {/* <!-- Put this part before </body> tag --> */}
         <input type="checkbox" id="my-modal-6" className="modal-toggle" />
         <div className="modal modal-bottom sm:modal-middle ">
           <div className="modal-box bg-white ">
+            <hr className='mb-4 mt-1 w-12 border-2 rounded mx-auto' />
+            <h4 className='font-bold text-lg'>Order Details</h4>
+            <hr className='my-4' />
             <p className="font-semibold">Order ID #123456</p>
             <p className="font-semibold">Delivery Address</p>
             <p>Lorem ipsum dolor sit amet.</p>
             <p className="font-semibold">Payment method</p>
             <p>Cash on delivery</p>
             <div className="modal-action ">
-              <label htmlFor="my-modal-6" className="btn bg-primary text-white">
+              <label htmlFor="my-modal-6" className="btn w-full bg-[#1750FF] border-0  text-white">
                 Done!
               </label>
             </div>
